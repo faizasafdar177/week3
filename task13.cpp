9e8aa7559d1bf8cd345ca82cdf266de4371c0120
faizasafdar177 <144409199+faizasafdar177@users.noreply.github.com>
2023-09-22 03:30:38 -0700

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"Enter number 1: ";
+int num1,sum=0;
+cin>>num1;
+sum=sum+num1;
+cout<<"Enter number 2: ";
+cin>>num1;
+sum=sum+num1;
+cout<<"Enter number 3: ";
+cin>>num1;
+sum=sum+num1;
+cout<<"Enter number 4: ";
+cin>>num1;
+sum=sum+num1;
+cout<<"Enter number 5: ";
+cin>>num1;
+sum=sum+num1;
+cout<<"The total sum is: "<<sum;
+}
+
+
+
+
+
