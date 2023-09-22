7e64421f492eaf463e71f521bf6b8501cbb23aa8
faizasafdar177 <144409199+faizasafdar177@users.noreply.github.com>
2023-09-22 02:46:54 -0700

    Add files via upload

---
+++ pftask09.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+int age;
+int days;
+cout<<"Enter your age in years: ";
+cin>>age;
+days=age*365;
+cout<<"Your age in days is approximately "<<days<<" days.";
+}
