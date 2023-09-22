77f12b9eaf7242b0a75116fa0c0a8d1c49de84bd
faizasafdar177 <144409199+faizasafdar177@users.noreply.github.com>
2023-09-22 04:51:55 -0700

    Add files via upload

---
+++ pdtask7.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+int hours;
+int seconds;
+cout<<"enter the no of hours: ";
+cin>>hours;
+seconds=hours*3600;
+cout<<hours<< "hours is equal to "<<seconds<<" seconds";
+}
---
+++ pdtask9.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){cout<<" ------------------------ "<<endl;
+       cout<<"   o   ^  ^               "<<endl;
+       cout<<"        __                "<<endl;
+       cout<<"    o  (oo)\__________    "<<endl;
+       cout<<"       (__)\          )\/\"<<endl;
+       cout<<"           ||-------w |   "<<endl;
+       cout<<"           ||        ||   "<<endl;
+}
